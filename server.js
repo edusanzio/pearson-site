af869d91d30b689aa24a540ca25e703a5af3871a
Eduardo Sanzio <eduardosanzio@gmail.com>
2025-09-19 15:48:49 -0300

    add server

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const { createServer } = require('http');
+const next = require('next');
+
+const port = process.env.PORT || 3000; // a KingHost injeta PORT
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => handle(req, res))
+    .listen(port, '0.0.0.0', () => {
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+});
